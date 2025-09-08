a056d5d1af891a4b9e2e5f0645f1686c1e1d1e89
Victor Díaz <vdiazrodriguez290@gmail.com>
2025-09-07 20:41:20 -0600

    Create MemoriaDinamica.py

---
+++ MemoriaDinamica.py
@@ -0,0 +1,13 @@
+frutas=[]
+
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")
+frutas.append("Uva")
+
+print(frutas)
+frutas.pop(0)
+frutas.pop(2)
+frutas.append("Sandia")
+
+print (frutas)
